09487bf1ae9d72397b6a16fab31c192cd3259b2d
Sergey Taranov <cept73@yandex.ru>
2024-03-03 12:50:59 +0300

    1st step - add base test

---
+++ tests/Feature/Api/PropertiesTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PropertiesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_get_all_properties()
+    {
+        // Create Property so that the response returns it.
+        $property = Property::factory()->create();
+
+        $response = $this->getJson(route('api.properties.index'));
+        // We will only assert that the response returns a 200 status for now.
+        $response->assertOk();
+    }
+}
